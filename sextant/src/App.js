909b0687d6385ffaa1c695bc1a65347f562fda16
Ademola Olalere <olalerer2@gmail.com>
2022-12-22 15:22:32 -0600

    A component that display the user public address

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from '../src/Components/Banner/Banner';
 import Exhibit from '../src/Components/Exhibit/Exhibit';
+import GetIp from './Components/RGetIp';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Banner bannerText="Sextant" />
-                <Exhibit name="I'm an exhibit!"></Exhibit>
-                <Exhibit name="My name is Ademola!"></Exhibit>
-                <Exhibit name="Frontend Development Track!"></Exhibit>
+                <Banner bannerText="Sextant App" />
+                <div>
+                    <Exhibit name="Ademola" heroName="Ace"></Exhibit>
+                    <Exhibit track=""></Exhibit>
+                    <Exhibit name=""></Exhibit>
+                    <GetIp />
+                </div>
             </div>
         );
     }
--- sextant/src/Components/Exhibit/Exhibit.js
+++ sextant/src/Components/Exhibit/Exhibit.js
@@ -5,9 +5,9 @@ class Exhibit extends Component {
     render() {
         return (
             <div className="Exhibit">
-                <h2 className="ExhibitHeading">{this.props.name}</h2>
+                <h2 className="ExhibitHeading">My name is {this.props.name}, Welcome {this.props.heroName} </h2>
                 <div className="ExhibitContent">
-                    {this.props.children}
+                    {this.props.children}                    
                 </div>
             </div>
         );
