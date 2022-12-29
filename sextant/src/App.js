64d7cd05d5aa533b7ab762d0d58457bdba2bfda0
Ademola Olalere <olalerer2@gmail.com>
2022-12-29 07:37:55 -0600

    Use WebSocket to connect to an external service.

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Banner from '../src/Components/Banner/Banner';
 import Exhibit from '../src/Components/Exhibit/Exhibit';
-import GetIp from './Components/RGetIp';
+import PylonWebSocket from './Components/PylonWebSocket';
+import IpAddress from './Components/IpAddress';
+
 
 class App extends Component {
     render() {
@@ -10,10 +12,13 @@ class App extends Component {
             <div className="App">
                 <Banner bannerText="Sextant App" />
                 <div>
-                    <Exhibit name="Ademola" heroName="Ace"></Exhibit>
-                    <Exhibit track=""></Exhibit>
-                    <Exhibit name=""></Exhibit>
-                    <GetIp />
+                    <Exhibit name="Ademola" heroName="Ace" /> 
+                    <Exhibit>
+                        <IpAddress />
+                    </Exhibit>
+                    <Exhibit>
+                        <PylonWebSocket />
+                    </Exhibit>
                 </div>
             </div>
         );
---
+++ sextant/src/Components/IpAddress.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+
+const IpAddress = () => {
+
+    async function getIP(){
+        const response = await fetch('https://api.ipify.org/?format=json');
+        const data = await response.json();
+        return response.status === 200 ? data : "err"
+    }
+
+    useEffect( () => {
+        getIP().then(data => console.log(data))
+    },[])
+
+    return (
+        <div>{getIP}</div> 
+    );
+};
+
+export default IpAddress
